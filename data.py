eb15866e5454b694a64803bd2ace4d059e0374b3
DACUS1995 <studormarian@gmail.com>
2020-05-08 20:22:06 +0300

    someting works

---
+++ data.py
@@ -0,0 +1,147 @@
+import numpy as np
+import pandas as pd
+from typing import Dict, List, Tuple
+import json
+import pathlib
+from PIL import Image
+import os
+
+from pytorch_detection.utils import collate_fn
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+from torchvision import datasets, models, transforms
+import pytorch_detection.transforms as T 
+from torch.utils.data.dataset import Dataset
+from torch.utils.data import DataLoader
+
+device = torch.device("cuda")
+
+class DataHandler:
+	def __init__(self, run_config):
+		self._training_dataset = None
+		self._validation_dataset = None
+		self._run_config = run_config
+		self.root_path = "./dataset"
+
+		self.load_datasets()
+		
+	def load_datasets(self):
+		masked_faces_paths = list(pathlib.Path(self.root_path + "/masked_faces").glob('*'))
+		normal_faces_paths = list(pathlib.Path(self.root_path + "/normal_faces").glob('*'))
+
+		ratio = 90
+		length = len(masked_faces_paths)
+		training_size = int(0.8 * length)
+
+		training_paths = masked_faces_paths[:training_size] + normal_faces_paths[:training_size]
+		validation_paths = masked_faces_paths[training_size:] + normal_faces_paths[training_size:]
+
+		self._training_dataset = CustomDataset(training_paths, root_path="./dataset", run_type="train")
+		self._validation_dataset = CustomDataset(validation_paths, root_path="./dataset", run_type="valid")
+
+	def get_data_loaders(self) -> Tuple[DataLoader]:
+		return (
+			DataLoader(
+				self._training_dataset, 
+				batch_size=self._run_config.batch_size, 
+				shuffle=True, 
+				num_workers=self._run_config.workers, 
+				pin_memory=False,
+				collate_fn=collate_fn
+			), 
+			DataLoader(
+				self._validation_dataset, 
+				batch_size=self._run_config.batch_size, 
+				shuffle=True, 
+				num_workers=self._run_config.workers, 
+				pin_memory=False,
+				collate_fn=collate_fn
+			)
+		)
+
+	def get_datasets(self) -> Tuple[Dataset]:
+		return self._training_dataset, self._validation_dataset
+
+	def get_datasets_sizes(self) -> Tuple[int]:
+		return len(self._training_dataset), len(self._validation_dataset)
+
+
+class CustomDataset(Dataset):
+	def __init__(self, image_paths, root_path = "./dataset", run_type = "train"):
+		self.root_path = root_path
+		self.run_type = run_type
+		with open(self.root_path + "/targets.json") as json_file:
+			self.targets = json.load(json_file)
+
+		self.images_paths = image_paths
+
+		self.transformers = {
+			'train_transforms' : transforms.Compose([
+				# transforms.RandomHorizontalFlip(0.5),
+				transforms.ToTensor()
+			]),
+			'valid_transforms' : transforms.Compose([
+				transforms.ToTensor()
+			])
+		}
+
+
+	def __getitem__(self, idx):
+		image_path = self.images_paths[idx]
+		image_name = str(image_path).split(os.sep)[2]
+		image_class = str(image_path).split(os.sep)[1]
+
+		class_to_label = {
+			"masked_faces": 0,
+			"normal_faces": 1
+		}
+
+		label = class_to_label[image_class]
+		image = np.array(Image.open(image_path).convert('RGB'))
+		# for bbox in self.targets[image_name]["bbox"]:
+		# 	bbox[0] = 
+		# 	bbox[1] = 
+		# 	bbox[2] = 
+		# 	bbox[3] = 
+
+		# annotations = {
+		# 	'image': image, 
+		# 	'bboxes': self.targets[image_name]["bbox"], 
+		# 	'category_id': [label]
+		# }
+		# aug = get_aug([Resize(p=1, height=224, width=224)])
+		# augmented = aug(**annotations)
+		# image = augmented['image']
+
+		target = {}
+		if len(self.targets[image_name]["bbox"]) == 0:
+			self.targets[image_name]["bbox"] = [[]]
+		target["boxes"] = torch.as_tensor(self.targets[image_name]["bbox"], dtype=torch.float32).to(device)
+		target["labels"] = torch.as_tensor([label] * len(self.targets[image_name]["bbox"]), dtype=torch.float32).to(device)
+		# target["masks"] = masks
+		# target["image_id"] = image_id
+		# target["area"] = area
+		target["iscrowd"] = torch.tensor([False]).to(device)
+
+		if self.transformers is not None:
+			image = self.transformers[self.run_type + "_transforms"](image)
+		return image, target
+
+
+	def __len__(self):
+		return len(self.images_paths)
+
+
+def get_aug(aug, min_area=0., min_visibility=0.):
+	return Compose(
+		aug, 
+		bbox_params=BboxParams(
+			format='pascal_voc', 
+			min_area=min_area, 
+			min_visibility=min_visibility, 
+			label_fields=['category_id']
+		)
+	)
---
+++ train_run_builder.py
@@ -0,0 +1,199 @@
+import numpy as np
+import pandas as pd
+import argparse
+from argparse import Namespace
+from collections import OrderedDict
+import time
+import os
+import copy
+import datetime
+from tqdm import tqdm
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.optim.lr_scheduler import StepLR
+from torch.utils.tensorboard import SummaryWriter
+from pytorch_detection.engine import train_one_epoch
+
+from run_builder import RunBuilder
+from data import DataHandler
+from config import Config
+import utils
+from models import model_rcnn
+
+torch.manual_seed(0)
+np.random.seed(0)
+# torch.backends.cudnn.deterministic = True
+# torch.backends.cudnn.benchmark = False
+
+
+def train(cfg) -> None:
+	device = torch.device(cfg.device)
+
+	runs = None
+	if cfg.use_run_setup == True:
+		runs = RunBuilder.get_runs(cfg.run_setup)
+	else:
+		runs = RunBuilder.get_runs(
+			OrderedDict({
+				"lr": [cfg.lr],
+				"num_epochs": [cfg.num_epochs]
+			})
+		)
+	assert runs != None
+
+
+	data_handler = DataHandler(cfg)
+	train_dataset, validation_dataset = data_handler.get_datasets()
+	train_loader, validation_loader = data_handler.get_data_loaders()
+	training_dataset_size, validation_dataset_size = data_handler.get_datasets_sizes()
+
+
+	best_model_wts = None
+	best_acc = 0.0
+	best_config = None
+
+	for run in runs:
+		comment = f"Run setup -- {run}"
+		print(comment)
+
+		model = model_rcnn.create_model(num_classes=2)
+		model.to(device)
+		optimizer = optim.Adam(model.parameters(), lr=0.0001, betas=(0.9, 0.999), eps=1e-08, weight_decay=1e-4)
+
+		# Check if resume
+		if cfg.use_run_setup == False and cfg.resume == True:
+			checkpoint = torch.load("./checkpoints/ckp.pt")
+			model.load_state_dict(checkpoint["model_state_dict"])
+			optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
+			run.num_epochs -= checkpoint["epoch"]
+
+		# loss_criterion = ...
+
+		log_dir = "logs/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+		writer = SummaryWriter(log_dir)
+
+		since = time.time()
+
+		for epoch in range(run.num_epochs):
+			print('Epoch {}/{}'.format(epoch, run.num_epochs))
+			print('-' * 10)
+
+			# train for one epoch, printing every 10 iterations
+			train_one_epoch(model, optimizer, train_loader, device, epoch, print_freq=10)
+			continue
+			
+
+			########### Training step ###########
+			model.train()
+			training_loss = []
+			running_loss = 0.0
+			running_corrects = 0
+			
+			for i, data in enumerate(tqdm(train_loader, desc=f"Epoch [{epoch + 1}] progress")):
+
+				x_batch, label_batch = data
+				x_batch, = x_batch.to(device)
+
+				optimizer.zero_grad()
+				outputs = model(x_batch)
+				_, preds = torch.max(outputs, 1)
+
+				loss = loss_criterion(outputs, label_batch)
+
+				loss.backward()
+				optimizer.step()
+				
+				# statistics
+				running_loss += loss.item() * x_batch.size(0)
+				running_corrects += torch.sum(preds == label_batch.detach())
+				training_loss.append(loss.item())
+
+				# # tensorboard logging
+				# if i % 1000 == 0:
+				# 	writer.add_scalar("Loss/train", running_loss / 1000, epoch * len(trainloader) + i)
+				# 	writer.add_scalar("Accuracy/train", running_loss / 1000, epoch * len(trainloader) + i)
+
+
+			epoch_loss = running_loss / training_dataset_size
+			epoch_acc = running_corrects.double() / training_dataset_size
+
+			# tensorboard logging
+			writer.add_scalar("Loss/train", epoch_loss, epoch)
+			writer.add_scalar("Accuracy/train", epoch_acc, epoch)
+
+			print('Training step => Loss: {:.4f} Acc: {:.4f}'.format(
+				epoch_loss, epoch_acc
+			))
+
+
+			########### Validation step ###########
+			model.eval()
+			validation_loss = []
+			running_loss = 0.0
+			running_corrects = 0
+
+			for i, data in enumerate(validation_loader):
+				with torch.no_grad():
+					x_batch, label_batch = data
+					x_batch, label_batch = x_batch.to(device), label_batch.to(device)
+
+					outputs = model(...)
+					_, preds = torch.max(outputs, 1)
+					loss = loss_criterion(...)
+
+					running_loss += loss.item() * x_batch.size(0)
+					running_corrects += torch.sum(preds == label_batch.detach())
+					validation_loss.append(loss.item())
+			
+			epoch_loss = running_loss / validation_dataset_size
+			epoch_acc = running_corrects.double() / validation_dataset_size
+
+			# tensorboard logging
+			writer.add_scalar("Loss/validation", epoch_loss, epoch)
+			writer.add_scalar("Accuracy/validation", epoch_acc, epoch)
+
+			print('Evaluation step => Loss: {:.4f} Acc: {:.4f}'.format(
+				epoch_loss, epoch_acc
+			))
+
+			#Save the best model based on accuracy
+			if epoch_acc > best_acc:
+				best_acc = epoch_acc
+				best_config = f"{run}"
+				best_model_wts = copy.deepcopy(model.state_dict())
+
+			#Checkpoint
+			torch.save({
+				"epoch": epoch,
+				"model_state_dict": model.state_dict(),
+				"optimizer_state_dict": optimizer.state_dict()
+			}, "./checkpoints/ckp.pt")
+
+
+		time_elapsed = time.time() - since
+		print('Training complete in {:.0f}m {:.0f}s'.format(
+			time_elapsed // 60, time_elapsed % 60
+		))
+		print('Best (so far) validation Acc: {:4f}'.format(best_acc))
+
+
+	print('-' * 10)
+	print('### Final results ###\n')
+	print('Best validation Acc: {:4f}'.format(best_acc))
+	print(f"Best configuration: {best_config}")
+
+	# model.load_state_dict(best_model_wts)
+	return model
+
+
+def main(args: Namespace) -> None:
+	trained_model = train(Config)
+	torch.save(model.state_dict(), f"model.pt")
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+	args = parser.parse_args()
+	main(args)
